76b1da7c50a21bc37dd08600e09c8130dc8e3213
shubhangi251 <mishra25anju10@gmail.com>
2020-03-02 00:11:51 +0530

    1 file modified

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,3 +1,6 @@
+
+
+
 import {Component} from '@angular/core';
 import {CoursesService} from './courses.service';
 
@@ -9,18 +12,15 @@ import {CoursesService} from './courses.service';
   <li *ngFor="let course of courses">
 {{course}}
   </li></ul>
-  <img src={{imageUrl}}/>
-  <img [src]="imageUrl"/>
-  <table>
-     <tr>
-      <td>
-        [colspan]="colSpan"
-     </td>
-    </tr>
-  </table>`
+  <input [value]="ans" (keyup.enter)="ans=$event.target.value; onPress()"/>
+   <input [(ngModel)]=ans (keyup.enter)="onPress()"/>`  //Two way binding
 })
 export class CoursesComponent
 {
+ ans="Tina";
+  onPress(){
+    console.log(this.ans);
+  }
 title="List of courses";
 imageUrl="https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-600w-407021107.jpg";
 colSpan=2;
