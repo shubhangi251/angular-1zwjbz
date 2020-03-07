855325300b201cc00d5cca9045176d7baed26f87
shubhangi251 <mishra25anju10@gmail.com>
2020-03-07 22:58:10 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -25,7 +25,7 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule ,ReactiveFormsModule],
   declarations: [ AppComponent, CoursesComponent, AuthorsComponent,
   SummaryPipe,
   CorrectcasePipe,
