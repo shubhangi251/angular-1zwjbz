8db6f00dae15621405334f625c4f1e10736fe844
shubhangi251 <mishra25anju10@gmail.com>
2020-03-07 23:51:39 +0530

    2 files modified

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
@@ -8,8 +8,13 @@ import { FormGroup,FormControl } from '@angular/forms';
 })
 export class SignupFormComponent {
   form=new FormGroup(
-    {username:new FormControl(),
-    password:new FormControl()
+    {
+      username: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required)
     }
   );
+  get username()
+  {
+    return this.form.get('username');
+  }
 }
