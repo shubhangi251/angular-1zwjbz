175bddbe7e4d599f553cefa1404aa3b298881242
shubhangi251 <mishra25anju10@gmail.com>
2020-03-09 01:30:34 -0400

    1 file modified

--- src/app/common/validators/username.validators.ts
+++ src/app/common/validators/username.validators.ts
@@ -3,7 +3,15 @@ export class UsernameValidators{
  static cannotContainSpace(control:AbstractControl): ValidationErrors|null
   {
 if((control.value as string).indexOf('')>=0)
-return{cannotContainSpace:true}
+return{cannotContainSpace:true};
+return null;
+  }
+  static shouldBeUnique(control:AbstractControl):ValidationErrors|null
+  {
+if(control.value=='mosh')
+return{
+  shouldBeUnique:true
+};
 return null;
   }
 }
