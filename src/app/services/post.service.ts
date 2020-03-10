543abb6ebe15073302cac7a2c9cca600413fe49a
shubhangi251 <mishra25anju10@gmail.com>
2020-03-10 16:02:06 +0530

    1 file modified

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
-import { AppError,NotFoundError} from './../common';
-
+import { AppError} from './../common/app-error';
+import { NotFoundError} from './../common/not-found-error';
 @Injectable()
 export class PostService {
 private url='https://jsonplaceholder.typicode.com/posts';
@@ -21,9 +21,10 @@ updatePosts(post)
  return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
 }
 deletePosts(id){
-return this.http.delete(this.url + '/' + id).catch((error:Response)=>
+return this.http.delete(this.url + '/' + id)
+.catch((error:Response)=>
 {
-  if(error:status ===404)
+  if(error.status ===404)
   return Observable.throw( new NotFoundError());
 
   return Observable.throw(new AppError(error));
