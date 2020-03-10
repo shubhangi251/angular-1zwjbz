558b9ca0a4801dedd0d4d28cc19a647ac1307845
shubhangi251 <mishra25anju10@gmail.com>
2020-03-10 18:36:43 +0530

    1 file modified

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/catch';
-import { AppError} from './../common/app-error';
-import { BadInput} from './../common/bad-input';
-import { NotFoundError} from './../common/not-found-error';
 import {DataService} from './data.service';
 
 @Injectable()
 export class PostService extends DataService{ 
-//;
-  constructor('https://jsonplaceholder.typicode.com/posts' , http:Http) {
-super(http);
+
+  constructor(http:Http) {
+super('http://jsonplaceholder.typicode.com/posts',http);
    }
 
 }
